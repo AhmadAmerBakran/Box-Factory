a09eb41aa2c1e758f8edafc386846b465afe4303
AhmadAmerBakran <ahmbak01@easv365.dk>
2023-10-10 11:50:03 +0200

    first playwright test added

---
+++ BoxFactoryTest/playwrightTests/PlaywrightTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace BoxFactoryTest.playwrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class PlaywrightTests : PageTest
+{
+    [Test]
+    public async Task HomepageHasDataBoxInTitle()
+    {
+        
+        await Page.GotoAsync("http://localhost:4200/");
+
+        await Expect(Page).ToHaveTitleAsync(new Regex("Data Box"));
+
+        var heading = Page.GetByRole(AriaRole.Heading);
+        await Expect(heading).ToHaveTextAsync(new Regex("Welcome to Box Factory project"));
+
+    }
+    
+    [Test]
+    public async Task HomepageHasHeading()
+    {
+        
+        await Page.GotoAsync("http://localhost:4200/");
+
+        var heading = Page.GetByRole(AriaRole.Heading);
+        await Expect(heading).ToHaveTextAsync(new Regex("Welcome to Box Factory project"));
+
+    }
+    
+}
